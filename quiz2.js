dcb927c81abf840adbb53d659e99793e1d6cb68a
Raghav Kathuria <raghavkathuria09@gmail.com>
2017-06-30 16:00:28 +0530

    funzone.html added

---
+++ quiz2.js
@@ -0,0 +1,93 @@
+var bttn =document.querySelector("button");
+bttn.onclick = function(){
+var s=0;
+var a=prompt("Who invented facebook?").toLowerCase();
+if(a==="mark zuckerberg"){
+s=s+1;
+alert("Right answer"); 
+}
+else {
+alert("Wrong answer");
+}
+var b=prompt("Who is the richest man in the world?").toLowerCase();
+if(b=="bill gates"){
+s=s+1;
+alert("Right answer");
+}
+else {
+alert("Wrong answer");
+}
+var c=prompt("Who is the 45th president of United States?").toLowerCase();
+if(c=="donald trump"){
+s=s+1;
+alert("Right answer");
+}
+else {
+
+alert("Wrong answer");
+}
+var d=prompt("Which is the largest continent of the world?").toLowerCase();
+if(d=="asia"){
+s=s+1;
+alert("Right answer");
+}
+else {
+
+alert("Wrong answer");
+}
+var e=prompt("which sport features no time clock?").toLowerCase();
+if(e=="cricket"){
+s=s+1;
+alert("Right answer");
+}
+else {
+
+alert("Wrong answer");
+}
+var f=prompt("which state is the leading producer of cement in India?").toLowerCase();
+if(f=="rajasthan"){
+s=s+1;
+alert("Right answer");
+}
+else {
+
+alert("Wrong answer");
+}
+var g=prompt("which was the first resedential International University of the world?").toLowerCase();
+if(g=="nalanda"){
+s=s+1;
+alert("Right answer");
+}
+else {
+
+alert("Wrong answer");
+}
+var h=prompt("who coined the term science?").toLowerCase();
+if(h=="william whewell"){
+s=s+1;
+alert("Right answer");
+}
+else {
+
+alert("Wrong answer");
+}
+var i=prompt("Who owns youtube?").toLowerCase();
+if(i=="google"){
+s=s+1;
+alert("Right answer");
+}
+else {
+
+alert("Wrong answer");
+}
+var j=prompt("which part of Inida do the kokani speaking people belong to?").toLowerCase();
+if(j=="goa"){
+s=s+1;
+alert("Right answer");
+}
+else {
+
+alert("Wrong answer");
+}
+alert("Your score is "+s);
+}
